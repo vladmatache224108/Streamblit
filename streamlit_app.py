acd7a4b594651ff1ae25c9da6c9e93b8df9388a8
vladmatache224108 <224108@buas.nl>
2023-05-16 11:13:00 +0200

    tep

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
+from matplotlib import pyplot as plt
 
-st.title('Uber pickups in NYC')
+st.title('Green Index and Livability Index by Neighbourhood')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = 'data/green_live.csv'
 
-@st.cache_data
+#@st.cache_data
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
 # Create a text element and let the reader know the data is loading.
@@ -29,17 +28,31 @@ if st.checkbox('Show raw data'):
     st.write(data)
 
 
-st.subheader('Number of pickups by hour')
-
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
 
-st.bar_chart(hist_values)
+grouped_data = data.groupby('year')['green_score','livability_score'].mean()
+grouped_data = grouped_data.reset_index()
+
+years = grouped_data['year'].astype('int').values.round(decimals = 2).tolist()
+green_score = grouped_data['green_score'].values.round(decimals = 2).tolist()
+live_score = grouped_data['livability_score'].values.tolist()
+
+grouped_data['year'] = years
+grouped_data['green_score'] = green_score
+grouped_data['livability_score'] = live_score
+
+a = st.text(grouped_data)
+
+st.subheader('Average green_score by year')
+
+st.line_chart(grouped_data, x = 'year', y = 'green_score')
+
+st.subheader('Average livability_score by year')
 
 
-hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
+fig, ax = plt.subplots()
+ax.plot(grouped_data['year'],grouped_data['livability_score'])
+ax.plot(grouped_data['year'],grouped_data['green_score'])
+ax.set_yscale('log')
+st.pyplot(fig)
+#st.map(data)
